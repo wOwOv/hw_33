e4d1c23e0bdf886270668527788303e7636a98c7
wOwOv <regret6458@gmail.com>
2025-03-21 15:25:11 +0900

    Add files via upload

--- Messagestub.cpp
+++ Messagestub.cpp
@@ -13,46 +13,61 @@ switch(type)
 case DEFPROCCREATEME:
 {
 ReqProcCreateMe(player,buf);
+break;
 }
 case DEFPROCCREATEOTHER:
 {
 ReqProcCreateOther(player,buf);
+break;
 }
 case DEFPROCCREATEOTHER:
 {
 ReqProcCreateOther(player,buf);
+break;
 }
 case DEFPROCDELETE:
 {
 ReqProcDelete(player,buf);
+break;
 }
 case DEFPROCMOVESTART:
 {
 ReqProcMoveStart(player,buf);
+break;
 }
 case DEFPROCMOVESTART:
 {
 ReqProcMoveStart(player,buf);
+break;
 }
 case DEFPROCMOVESTOP:
 {
 ReqProcMoveStop(player,buf);
+break;
 }
 case DEFPROCATTACK1:
 {
 ReqProcAttack1(player,buf);
+break;
 }
 case DEFPROCATTACK2:
 {
 ReqProcAttack2(player,buf);
+break;
 }
 case DEFPROCATTACK3:
 {
 ReqProcAttack3(player,buf);
+break;
 }
 case DEFPROCDAMAGE:
 {
 ReqProcDamage(player,buf);
+break;
+}
+default:
+{
+break;
 }
 }
 
